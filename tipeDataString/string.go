6eef4ca77686e4f1f11250da6cef96a29b463de1
Farrid Kuntoro <farridguntoro@gmail.com>
2022-02-04 11:20:19 +0700

    [Learn] - New Path: Code Program String

--- tipeDataString/string.go
+++ tipeDataString/string.go
@@ -6,4 +6,9 @@ func main() {
 	fmt.Println("Farrid")
 	fmt.Println("Farrid Kun")
 	fmt.Println("Farrid Kuntoro")
+
+	// Kode Program String
+	fmt.Println(len("Farrid"))
+	fmt.Println("Farrid Kun"[0])
+	fmt.Println("Farrid Kuntoro"[1])
 }
